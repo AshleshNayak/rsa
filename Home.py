5f933ccb5ceec2318f2dd021ebb6b4b28e573b8f
Ashlesh V Nayak <143422745+AshleshNayak@users.noreply.github.com>
2025-10-07 19:35:29 +0530

    Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.title("Implement RSA Cryptanalysis (small factorization attack)")
 st.markdown("""
 This application demonstrates a fundamental cryptanalytic attack against RSA, known as the **"small factorization attack."**
 
-Its security relies on the practical difficulty of factoring the product of two very large prime numbers. This demo shows what happens when that principle is ignored by using tiny, 2-digit primes.
+Its security relies on the practical difficulty of factoring the product of two very small prime numbers. This demo shows what happens when that principle is ignored by using tiny, 2-digit primes.
 
 
 """)
