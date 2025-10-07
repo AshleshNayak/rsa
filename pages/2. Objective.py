d527a542e4e62891503744732f8475367e18bc58
Ashlesh V Nayak <143422745+AshleshNayak@users.noreply.github.com>
2025-10-07 19:37:32 +0530

    2. Objective.py

--- pages/2. Objective.py
+++ pages/2. Objective.py
@@ -4,7 +4,7 @@ st.header("Objective")
 st.markdown("---")
 
 st.markdown("""
-The objective of this demonstration is to illustrate how an attacker, given only a public RSA key (`e`, `n`), can recover the corresponding private key (`d`, `n`) if the modulus `n` is small enough to be factored.
+The objective of this demonstration is to illustrate how an attacker, given only a public RSA key (`e`, `n`), can recover the corresponding private key (`d`, `n`) if the value `n` is small enough to be factored.
 
 This serves as an educational tool to highlight the foundational security principle of RSA: **the intractability of integer factorization for large numbers.**
 """)
